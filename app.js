b0151d13d380fab713aa1da367629a2bc6deb428
pixalynx <pixalynx@gmail.com>
2018-05-09 01:06:41 +1000

    Tested the data model to the console and it worked

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const _ = require('lodash');
 const settings = require('./settings').Setting;
+const DataModel = require('./DataModel/dataModel').DataModel;
+
 
 console.log(settings.startupMsg);
+console.log(DataModel.types[1]);
 //Our Server class which we are starting on port 5000
 const serverClass = require('./server');
 const server = new serverClass(5000);
