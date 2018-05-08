4b67956f80dfbb8597bfdec7dde71ba65e41d946
pixalynx <pixalynx@gmail.com>
2018-05-09 01:06:26 +1000

    Added the different types of products

---
+++ DataModel/dataModel.js
@@ -0,0 +1,10 @@
+// This is our datamodel for our database
+var DataModel = {
+    types : ["Laptop","Computer","TV","Mobile","Console","Drone","Camera"]
+}
+
+var test = {
+    name : 'lol'
+}
+
+module.exports.DataModel = DataModel;
