85570b778e5597aee5773c37f35e1f75232c40f9
pixalynx <pixalynx@gmail.com>
2018-05-09 01:06:06 +1000

    Created accountsModel so we can enter data in the accounts collection

---
+++ DataModel/accountsModel.js
@@ -0,0 +1,9 @@
+var accountsModel = {
+    username : string,
+    password : string, //this will be hashed
+    email : string,
+    mobile : number,
+    rankCode : number,
+    isVerified : boolean,
+    profilePicutre : string,   
+}
